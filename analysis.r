f4d19964c9f5c2bf800c4268a2f2f8eaa0f477f7
leul71 <leulazezew@gmail.com>
2022-08-17 11:20:41 -0700

    first

---
+++ analysis.r
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(maps)
+library(mapproj)
+library(plotly)
+soccer_df <- read.csv("https://raw.githubusercontent.com/tara-nguyen/english-premier-league-datasets-for-10-seasons/main/epldat10seasons/epl-allseasons-matchstats.csv")
